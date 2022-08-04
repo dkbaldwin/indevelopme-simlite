489ac736ab7fa707f642b3c0308808dbd86d6c07
dkbaldwin <dkbaldwin@indevelopme.com>
2022-08-04 04:45:20 -0500

    start.sh
    added comments and todo tag

--- start.sh
+++ start.sh
@@ -7,9 +7,18 @@ internal_port="80"
 container_name="simcontainer"
 
 
+#build the image
 docker build -t ${image_name} .
+################################
+#todo: need to create docker-compose.yml
+# file to remove the need of the following lines
+################################
+
+#stop the old container
 docker stop ${container_name}
+#delete the old container
 docker rm ${container_name}
+#create a new container
 docker run -d -p ${external_port}:${internal_port} \
   --name=${container_name} ${image_name}
   #  -v $PWD:/app \
